2d1e402b25182a1972b90f76fa423222fec7cd51
flaviovidalhuax <flaviovidalhuax@gmail.com>
2022-12-08 18:50:11 -0600

    fist commit

--- src/users/users.router.js
+++ src/users/users.router.js
@@ -5,8 +5,8 @@ const userServices = require('./users.services')
 router.get("/", JwtStrategy.authenticate('jwt', {session:false}) ,userServices.getAllUsers) //? /api/v1/users
 router.post("/", userServices.postUser) //? /api/v1/users
 
-router.get("/:id" ,userServices.getUserById) //? /api/v1/users/:id
-router.patch('/:id', userServices.patchUser) //? /api/v1/users/:id
-router.delete('/:id' ,userServices.deleteUser) //? /api/v1/users/:id
+router.get("/:id" ,JwtStrategy.authenticate('jwt', {session:false})  ,userServices.getUserById) //? /api/v1/users/:id
+router.patch('/:id', JwtStrategy.authenticate('jwt', {session:false}) ,userServices.patchUser) //? /api/v1/users/:id
+router.delete('/:id',JwtStrategy.authenticate('jwt', {session:false})  ,userServices.deleteUser) //? /api/v1/users/:id
 
 module.exports = router
